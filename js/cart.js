768e3976674ea9d2071d35524c15de1d54e3acd4
RakibHossain621 <mailto:rakibhasanrasel07@gmail.com>
2024-01-25 16:02:40 +0600

    finish today task

---
+++ js/cart.js
@@ -0,0 +1,30 @@
+// Menu Start
+const menuButton = document.querySelector(".hamburger_menu");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".overlay");
+const close_button = document.querySelector(".close_button");
+// Menu End
+
+
+// Menu 
+function disableScroll() {
+  document.body.style.overflow = "hidden";
+}
+function enableScroll() {
+  document.body.style.overflow = "auto";
+}
+
+function closeMenu() {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+  enableScroll();
+}
+
+menuButton.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
+  disableScroll();
+});
+
+close_button.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
